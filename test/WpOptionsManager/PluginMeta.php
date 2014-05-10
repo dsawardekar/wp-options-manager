69db34ba2396183c0182b2b1f44867ada5f15a2e
Darshan Sawardekar <darshan@sawardekar.org>
2014-05-10 11:36:16 +0530

    Adds options url.

--- test/WpOptionsManager/PluginMeta.php
+++ test/WpOptionsManager/PluginMeta.php
@@ -6,13 +6,28 @@ class PluginMeta {
 
   public $optionsKey;
   public $defaultOptions;
+  public $slug;
+
+  function getSlug() {
+    return $this->slug;
+  }
 
   function getOptionsKey() {
     return $this->optionsKey;
   }
 
+  function getOptionsCapability() {
+    return $this->optionsCapability;
+  }
+
   function getDefaultOptions() {
     return $this->defaultOptions;
+  }
+
+  function getOptionsUrl() {
+    return admin_url(
+      'options-general.php?page=' . $this->getSlug()
+    );
   }
 
 }
